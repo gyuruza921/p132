afaa4d0eadcf4e04aa5233684d9c2ba55c5b8fc1
gyurza921 <objekt.172.uru@gmail.com>
2020-07-31 08:33:21 +0900

    本及び本棚クラスを作成

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 "use strict"
+
+// 
+// 本及び本棚クラスを作成し本棚クラスに検索メソッドを作成する
+// 
+
+// 本クラス
+class Book {
+    constructor(title, author){
+
+        this.title = title;
+
+        this.author = author;
+
+    }
+}
+
+// 本棚クラス
+class Bookshelf {
+
+    constructor(){
+
+        this._books = [];
+
+    }
+
+    addBook(book) {
+
+        this._books.push(book);
+        
+    }
+
+}
